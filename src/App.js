361220423e729d271625608843e35f954c4a47b3
AlexandrKirichenko <kiri4enkoalexandr@gmail.com>
2021-07-06 10:14:16 +0300

    'add axios'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from "react";
+import axios from "axios";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer"
 
 function App() {
+
+    const [items, setItems] = React.useState([]);
+    const [cartItems, setCartItems]= React.useState([]);
+    const [searchValue,setSearchValue] = React.useState('');
+    const [cartOpened, setCartOpened] = React.useState(false);
+
+    // const onAddToCart = (obj) => {
+    //
+    //     const itemIndex = cartItems.findIndex(cartItem => cartItem.id === obj.id)
+    //
+    //     if(itemIndex < 0){
+    //         const newItem = {
+    //             ...obj,
+    //             quantity: 1,
+    //         }
+    //         setCartItems([...cartItems, newItem]);
+    //     } else {
+    //         const newCartItem = cartItems.map((cartItem, index) => {
+    //             if(index === itemIndex) {
+    //                 return {
+    //                     ...cartItem,
+    //                     quantity: cartItem.quantity + 1
+    //                 }
+    //             } else {
+    //                 return cartItem;
+    //             }
+    //         })
+    //
+    //         setCartItems(newCartItem);
+    //     }
+    //
+    // }
+
+    const onChangeSearchInput = (event) => {
+        setSearchValue(event.target.value);
+    }
+
+    React.useEffect(() => {
+
+        axios.get('https://60e06e046b689e001788ca6f.mockapi.io/items').then(res => {
+            setItems(res.data)
+        });
+
+    }, []);
+
+    const onAddToCart = (obj) => {
+        axios.post('https://60e06e046b689e001788ca6f.mockapi.io/cart',obj)
+        setCartItems(prev =>[...prev, obj]);
+    }
+
     return (
         <div className="wrapper clear">
-            <Drawer />
-            <Header />
+            {cartOpened && <Drawer items={cartItems}  onClose={() => setCartOpened(false)}/>}
+            <Header onClickCart={() => setCartOpened(true)}/>
             <div className="content p-40">
                 <div className="d-flex align-center justify-between mb-40">
-                    <h1>Все фрукты</h1>
+                    <h1>{ searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все фрукты' }</h1>
                     <div className="search-block d-flex">
                         <img src="/img/search.svg" alt="Search"/>
-                        <input placeholder="Поиск..."/>
+                        {searchValue && <img onClick={() => setSearchValue('')} className="clea cu-p" src="/img/btn-remove.svg" alt="Close"/>}
+                        <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."/>
                     </div>
                 </div>
 
 
-                <div className="cards d-flex">
-                    <Card />
-
+                <div className="cards d-flex flex-wrap">
+                    {items.filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+                        .map((item) => (
+                        <Card
+                            key={item.id}
+                            title={item.title}
+                            price={item.price}
+                            imageUrl={item.imageUrl}
+                            onPlus={(obj) => onAddToCart(obj)}
+                        />
+                    ))}
                 </div>
 
 
@@ -34,31 +94,3 @@ export default App;
 
 
 
-// <div className="card">
-//     <img width={200} height={170} src="https://lifeglobe.net/x/entry/6259/1a-0.jpg" alt="apple"/>
-//     <h5>Банан</h5>
-//     <div className="d-flex justify-between align-center">
-//         <div className="d-flex flex-column ">
-//             <span>Цена:</span>
-//             <b>10$</b>
-//         </div>
-//         <button className="button">
-//             <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
-//         </button>
-//     </div>
-// </div>
-// <div className="card">
-//     <img width={200} height={170}
-//          src="https://ideas-center.com.ua/wp-content/uploads/fc7146c85cb3d97069131e62c0fbfe27.png"
-//          alt="banana"/>
-//     <h5>Банан</h5>
-//     <div className="d-flex justify-between align-center">
-//         <div className="d-flex flex-column ">
-//             <span>Цена:</span>
-//             <b>10$</b>
-//         </div>
-//         <button className="button">
-//             <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
-//         </button>
-//     </div>
-// </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,40 +1,31 @@
-function Drawer() {
+function Drawer({onClose, items = [], quantity = 0}) {
     return (
-        <div style={{display:'none'}} className="overlay">
+        <div className="overlay">
             <div className="drawer d-flex flex-column">
                 <h2 className="d-flex justify-between mb-30">
                     Корзина
-                    <img className="cu-p" src="/img/btn-remove.svg" alt="Remove"/>
+                    <img onClick={onClose} className="cu-p" src="/img/btn-remove.svg" alt="Close"/>
                 </h2>
 
                 <div className="items flex">
-                    <div className="cartItem d-flex align-center mb-20">
-                        <img className="mr-20 p-5"
-                             width={70} height={70}
-                             src="https://ambassador-city-jomtien.ru/uploads/posts/2014-06/1402862895_banana2.jpg"
-                             alt="banana"/>
-                        <div className="mr-50">
-                            <p className="mb-5">Банан Кавендиш</p>
-                            <b>10$</b>
-                        </div>
-                        <img className="cardBtn" width={11} height={11} src="/img/plus.svg" alt="plus"/>
-                        <img className="cardBtn" width={11} height={11} src="/img/min.svg" alt="minus"/>
-                        <img className="cardBtn" src="/img/btn-remove.svg" alt="Remove"/>
-                    </div>
-
-                    <div className="cartItem d-flex align-center mb-20">
-                        <img className="mr-20 p-5"
-                             width={70} height={70}
-                             src="https://ambassador-city-jomtien.ru/uploads/posts/2014-06/1402862895_banana2.jpg"
-                             alt="banana"/>
-                        <div className="mr-50">
-                            <p className="mb-5">Банан Кавендиш</p>
-                            <b>10$</b>
-                        </div>
-                        <img className="cardBtn" width={11} height={11} src="/img/plus.svg" alt="plus"/>
-                        <img className="cardBtn" width={11} height={11} src="/img/min.svg" alt="minus"/>
-                        <img className="cardBtn" src="/img/btn-remove.svg" alt="Remove"/>
-                    </div>
+                    {
+                        items.map( (item)=> (
+                            <div key={item.id} className="cartItem d-flex align-center mb-20">
+                                <img className="mr-20 p-5"
+                                     width={70} height={70}
+                                     src={item.imageUrl}
+                                     alt="banana"/>
+                                <div className="mr-50">
+                                    <p className="mb-5">{item.title}</p>
+                                    <b>{item.price}$</b>
+                                </div>
+                                <img className="cardBtn" width={11} height={11} src="/img/plus.svg" alt="plus"/>
+                                {quantity ? <span>{quantity}</span> : null}
+                                <img className="cardBtn" width={11} height={11} src="/img/min.svg" alt="minus"/>
+                                <img className="cardBtn" src="/img/btn-remove.svg" alt="Remove"/>
+                            </div>
+                        ))
+                    }
                 </div>
                 <div className="cartTotalBlock">
                     <ul>
